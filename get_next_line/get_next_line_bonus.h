a3b5cc35c857582e5a469cbed2c7018cd10bafd8
Hannah Humaira Binti Darhaysyam Al Jefri <git@vogsphere.42iskandarputeri.edu.my:vogsphere/intra-uuid-40024920-4c5d-4207-9a18-bbce19696a20-6497739-hbinti-d>
2025-11-09 17:04:34 +0800

    gnl

---
+++ get_next_line/get_next_line_bonus.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbinti-d <hbinti-d@student.42iskandar      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/09 15:49:37 by hbinti-d          #+#    #+#             */
+/*   Updated: 2025/11/09 15:55:51 by hbinti-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *string, int searchedChar);
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t elementCount, size_t elementSize);
+size_t	ft_strlen(const char *theString);
+
+#endif
